108ffc00624adeafa72a7aebc113f6bb4c0f9ad9
Ticia <ticiarodela@gmail.com>
2020-05-02 14:14:41 -0700

    added pcitures

---
+++ App/Data/friends.js
@@ -0,0 +1,54 @@
+var friends = [ 
+        {
+            "name": " Chandler Bing ",
+            "photo": " https://images.app.goo.gl/fdWyttZpyK1Xki1F8 ",
+            "scores": [
+                5,
+                4,
+                2,
+                1,
+                1,
+                1,
+                3,
+                5,
+                5,
+                5,
+            ]
+        },
+        {
+            "name": " Phoebe Buffay ",
+            "Photo": " https://images.app.goo.gl/tU9nVHZnYt4UTRrf9 ",
+            "scores": [
+                5,
+                5,
+                3,
+                1,
+                3,
+                3,
+                5,
+                5,
+                5,
+                5,
+            ]
+        },
+    
+        { 
+            "name": "Gunther",
+            "photo": " https://images.app.goo.gl/8F3k6ERjCZWBQhVs6 ",
+            "scores": [
+                2,
+                2,
+                2,
+                3,
+                4,
+                4,
+                5,
+                5,
+                2,
+                2,
+    
+            ]
+            
+        }
+    ];
+    module.exports = friends;
